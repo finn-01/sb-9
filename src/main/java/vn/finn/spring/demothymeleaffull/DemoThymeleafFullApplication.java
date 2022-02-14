a962b723c6af10b65f573cee237142fb14726e67
finn-01 <nguyensam576@gmail.com>
2022-02-15 00:23:52 +0700

    set up

---
+++ src/main/java/vn/finn/spring/demothymeleaffull/DemoThymeleafFullApplication.java
@@ -0,0 +1,13 @@
+package vn.finn.spring.demothymeleaffull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoThymeleafFullApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoThymeleafFullApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/finn/spring/demothymeleaffull/DemoThymeleafFullApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.finn.spring.demothymeleaffull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoThymeleafFullApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
